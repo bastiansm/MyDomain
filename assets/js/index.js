8a28ed7eaa1c1749f74cc5367973f8904ad9f0b8
blog-pribadi <blogpribadi.contact@gmail.com>
2023-07-29 21:25:37 +0700

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,24 @@
+window.addEventListener("scroll", function () {
+    var navbar = document.querySelector(".navbar");
+    navbar.classList.toggle("sticky", window.scrollY > 0);
+})
+
+
+// swiper
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
